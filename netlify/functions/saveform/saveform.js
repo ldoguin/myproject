92bf832ed88029de6bd6dbb8c97b961ca14a5c05
Laurent Doguin <laurent.doguin@gmail.com>
2023-08-05 11:43:30 +0200

    send the form parameters to the function

--- netlify/functions/saveform/saveform.js
+++ netlify/functions/saveform/saveform.js
@@ -1,13 +1,9 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    var data = JSON.parse(event.body);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ name: data.name })
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
